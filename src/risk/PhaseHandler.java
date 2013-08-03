9155c360000617300e3ade161ed83d0f258b4543
Daniel Glasgow <daniel.n.glasgow@gmail.com>
2013-08-03 16:04:48 -0400

    Added TODO comments

--- src/risk/PhaseHandler.java
+++ src/risk/PhaseHandler.java
@@ -4,7 +4,15 @@ import java.util.concurrent.CountDownLatch;
 
 public class PhaseHandler {
 
+	// TODO(dani): Both of these class members should be private.
+	// TODO(dani): Write a function setNextPhase(....);
 	protected Phase nextPhase = null;
+	// TODO(dani): Write a function countDown(), or better yet, write a function
+	// void finishPhase(Phase nextPhase) { .... }; Then everything can be
+	// private,
+	// and you'll only have one function. The classes which "extend"
+	// PhaseHandler will
+	// not know the details of the implementation -- and they shouldn't.
 	protected final CountDownLatch latch = new CountDownLatch(1);
 
 	public Phase await() {
@@ -17,11 +25,19 @@ public class PhaseHandler {
 	}
 
 	// To be overriden.. should I make an interface?
+	// TODO(dani): Make this abstract.
+	// http://docs.oracle.com/javase/tutorial/java/IandI/abstract.html
+	// TODO(dani): Rename this to be: mouseClicked(Territory territory). It will
+	// make more sense.
 	public void action(Territory territory) {
 
 	}
 
-	// To be overriden.. should I make an interface?
+	// To be overridden.. should I make an interface?
+	// TODO(dani): Make this abstract.
+	// http://docs.oracle.com/javase/tutorial/java/IandI/abstract.html
+	// TODO(dani): Do you really want to call this "setInterface"?
+	// displayInterface()?
 	public void setInterface() {
 
 	}
